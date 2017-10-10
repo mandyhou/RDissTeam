26358a6a724ddf2350fa2cffe0a753ded315f08c
dectrfov <r05921014@ntu.edu.tw>
2017-10-10 20:09:59 +0800

    Create our_environments.R
    Operating system of each member

---
+++ our_environments.R
@@ -0,0 +1,8 @@
+list(
+    Hao = <x86_64-w64-mingw32/x64 (64-bit)>,
+    Danny = <x86_64-w64-mingw32/x64 (64-bit)>,
+    Hung = <x86_64-apple-darwin15.6.0 (64-bit)>,
+    Wang = <x86_64-w64-mingw32/x64 (64-bit)>,
+    Chang = <x86_64-apple-darwin15.6.0 (64-bit)>,
+    Hua = <x86_64-w64-mingw32/x64 (64-bit)>
+)
